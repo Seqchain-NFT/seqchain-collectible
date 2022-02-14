12ee9c03f8af5935809efc837e1ae4bc7fcacd83
gfijrb <gfijrb@yandex.ua>
2022-02-14 17:37:56 +0200

    add dynamical max supply for sequoia NFT

--- contracts/SeqchainSequoiaNFT.sol
+++ contracts/SeqchainSequoiaNFT.sol
@@ -6,6 +6,7 @@ import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 import { ERC721Enumerable, ERC721, Strings } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "./interfaces/ISequoiaNFT.sol";
 import "./access/OperatorAccess.sol";
+import "./libs/BokkyPooBahsDateTimeLibrary.sol";
 
 /**
  * @title SeqchainSequoiaNFT - General Seqchain NFT collection.
@@ -14,6 +15,7 @@ import "./access/OperatorAccess.sol";
 contract SeqchainSequoiaNFT is ISequoiaNFT, ERC721Enumerable, OperatorAccess {
     using Strings for uint256;
 
+    uint256 public initDate;
     uint256 public tokenIdTracker;
 
     string internal _baseUri;
@@ -24,7 +26,9 @@ contract SeqchainSequoiaNFT is ISequoiaNFT, ERC721Enumerable, OperatorAccess {
      */
     constructor()
         ERC721("Seqchain Sequoia NFT", "SEQNFT")
-    {}
+    {
+        initDate = block.timestamp;
+    }
 
     /**
      * @dev Mint NFTs token to specific address `_to`.
@@ -35,6 +39,9 @@ contract SeqchainSequoiaNFT is ISequoiaNFT, ERC721Enumerable, OperatorAccess {
      */
     function mint(address _to, uint256 _amount) external override onlyOperator {
         uint256 tokenId = tokenIdTracker;
+
+        require(tokenId + _amount < _countMaxSupply(), "Max supply reached");
+
         for (uint i; i < _amount; i++) {
             _mint(_to, tokenId + i);
         }
@@ -56,6 +63,34 @@ contract SeqchainSequoiaNFT is ISequoiaNFT, ERC721Enumerable, OperatorAccess {
         return _exists(_tokenId);
     }
 
+    function maxSupply() external view returns (uint supply) {
+        supply = _countMaxSupply();
+    }
+
+    function lastUnlock() external view returns (uint unlockedAt) {
+        uint256 _initDate = initDate;
+        uint256 yearsLeft = BokkyPooBahsDateTimeLibrary.diffYears(_initDate, block.timestamp);
+
+        if (yearsLeft < 2) {
+            unlockedAt = _initDate;
+        } else {
+            uint256 addYears = (yearsLeft % 2 != 0) ? (yearsLeft - 1) : yearsLeft - 2;
+            unlockedAt = BokkyPooBahsDateTimeLibrary.addYears(_initDate, addYears);
+        }
+    }
+
+    function nextUnlock() external view returns (uint unlockAt) {
+        uint256 _initDate = initDate;
+        uint256 yearsLeft = BokkyPooBahsDateTimeLibrary.diffYears(_initDate, block.timestamp);
+
+        if (yearsLeft < 2) {
+            unlockAt = _initDate;
+        } else {
+            uint256 addYears = (yearsLeft % 2 != 0) ? (yearsLeft + 1) : yearsLeft + 2;
+            unlockAt = BokkyPooBahsDateTimeLibrary.addYears(_initDate, addYears);
+        }
+    }
+
     /**
      * @dev See {IERC721Metadata-tokenURI}.
      */
@@ -75,5 +110,18 @@ contract SeqchainSequoiaNFT is ISequoiaNFT, ERC721Enumerable, OperatorAccess {
             bytes(_baseUri).length > 0
                 ? string(abi.encodePacked(_baseUri, (_tokenId).toString()))
                 : "";
+    }
+
+    function _countMaxSupply() internal view returns (uint supply) {
+        uint256 initAmount = 1000;
+        uint256 yearsLeft = BokkyPooBahsDateTimeLibrary.diffYears(initDate, block.timestamp);
+
+        if (yearsLeft < 2) {
+            supply = initAmount;
+        } else {
+            supply = initAmount + initAmount * (
+                (yearsLeft % 2 != 0) ? (yearsLeft - 1) : yearsLeft
+            );
+        }
     }
 }
